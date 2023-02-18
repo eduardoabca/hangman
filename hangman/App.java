4315d1ceb53fea50c9a37cc787de44acaf2a71d3
Eduardo Abreu Campos <70921963+eduardoabca@users.noreply.github.com>
2023-02-18 07:53:38 -0300

    Add files via upload

---
+++ hangman/App.java
@@ -0,0 +1,9 @@
+package hangman;
+
+import hangman.core.Game;
+
+public class App {
+    public static void main(String[] args) {
+        new Game().start();
+    }
+}
---
+++ hangman/core/InvalidLetterException.java
@@ -0,0 +1,7 @@
+package hangman.core;
+
+public class InvalidLetterException extends Exception {
+    public InvalidLetterException(String message) {
+        super(message);
+    }
+}
